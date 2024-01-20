c709b5d36896cc229be57adf763e1bfa73d8b7a3
Agud Ramon <agudadrian92@gmail.com>
2024-01-20 16:11:03 +0200

    Installed sass css preprocessor

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
 <template>
   <div>
     <p>Current route: {{ route.path }}</p>
     <v-btn color="red">Hello</v-btn>
   </div>
 </template>
+
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<style scoped lang="scss">
+</style>
