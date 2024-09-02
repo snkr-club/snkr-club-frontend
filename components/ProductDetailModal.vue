4a62226bb899a370c1bce0d09ea4ae5d8ee6679a
Agud Ramon <67280933+ZenAl3x@users.noreply.github.com>
2024-09-02 15:41:06 +0300

    Product detail modal product variant table

--- components/ProductDetailModal.vue
+++ components/ProductDetailModal.vue
@@ -2,6 +2,32 @@
     <v-dialog v-model="dialogModel" max-width="1300">
         <v-card>
             <v-card-title>Detalii produs</v-card-title>
+            <v-card-text class="d-flex align-center justify-start ga-2 flex-wrap">
+                <v-card
+                    width="100"
+                    height="100"
+                    class="pa-0 image-card"
+                    elevation="0"
+                >
+                    <v-img src="../public/snkr-lazyload.png"></v-img>
+                    <v-btn
+                        icon="mdi-close"
+                        variant="text"
+                        color="red"
+                    ></v-btn>
+                </v-card>
+                <v-btn
+                    variant="tonal"
+                    width="100"
+                    height="100"
+                >
+                    <v-icon size="45">mdi-image-plus</v-icon>
+                    <v-tooltip
+                        activator="parent"
+                        text="Adauga imagine"
+                    ></v-tooltip>
+                </v-btn>
+            </v-card-text>
             <v-card-text>
                 <v-row>
                     <v-col cols="6">
@@ -17,16 +43,22 @@
                 </v-row>
                 
                 <v-row>
-                    <v-col cols="6">
+                    <v-col cols="4">
                         <v-select
                             label="Tip produs"
                             :items="['BARBATI', 'FEMEI', 'UNISEX']"
                         ></v-select>
                     </v-col>
-                    <v-col cols="6">
+                    <v-col cols="4">
                         <v-text-field
                             label="Pret"
                         ></v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-select
+                            label="Status"
+                            :items="['ACTIV', 'INACTIV']"
+                        ></v-select>
                     </v-col>
                 </v-row>
                 
@@ -54,7 +86,11 @@
                 >
                 </v-textarea>
                 
-                <v-data-table></v-data-table>
+                <h4 class="my-3">Variante produs</h4>
+                
+                <v-data-table
+                    :headers="headers"
+                ></v-data-table>
             </v-card-text>
 <!--            <v-card-actions>-->
 <!--                <v-spacer />-->
@@ -77,6 +113,18 @@ const emit = defineEmits([
 
 const dialogModel = ref(props.showDialog)
 
+const headers = ref([
+    {
+        title: 'ID',
+        align: 'start',
+        key: 'id'
+    },
+    { title: 'Titlu', key: 'title' },
+    { title: 'Tip', key: 'type' },
+    { title: 'Pret', key: 'price' },
+    { title: 'Status', key: 'status' },
+])
+
 watch(() => props.showDialog, (newVal) => {
     dialogModel.value = newVal
 })
@@ -87,5 +135,17 @@ watch(() => dialogModel.value, (newVal) => {
 </script>
 
 <style scoped lang="scss">
+.v-img {
+    height: 100% !important;
+    width: 100%;
+}
 
+.image-card {
+    position: relative;
+    .v-btn {
+        position: absolute;
+        top: -6px;
+        right: -6px;
+    }
+}
 </style>
