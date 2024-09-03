fc266f18267b60ee5a121126649a579fba300df8
Agud Ramon <67280933+ZenAl3x@users.noreply.github.com>
2024-09-03 14:10:35 +0300

    Added vue toastification to the project

---
+++ plugins/toastification.client.ts
@@ -0,0 +1,11 @@
+import { defineNuxtPlugin } from '#app';
+import Toast, { PluginOptions } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const options: PluginOptions = {
+        // You can set your default options here
+    };
+
+    nuxtApp.vueApp.use(Toast, options);
+});
--- stores/productStore.js
+++ stores/productStore.js
@@ -7,28 +7,32 @@ export const useProductStore = defineStore('products', {
 		selectedProduct: null
 	}),
 	actions: {
-		async find(type = null, idList = null) {
-			this.products = []
-			let errors, data
-			console.log(type)
-			await fetch("http://192.168.0.115:6969/products/search", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify({
-					type: type,
-					idList: idList,
-				}),
-			}).then((res) => res.json()).then((jsonRes) => data = jsonRes).catch((err) => errors = err)
-			if (!data.success) return "Eroare interna."
-			if (!data.body.length) return 'Eroare interna'
-			if (idList && idList.length === 1) this.selectedProduct = data.body[0]
-			else this.products = data.body
-			this.availableSizes = data.available_sizes
-			console.log('Found products', this.products)
-			console.log('Found sizes', this.availableSizes)
-			if (errors) return errors
+		// async find(type = null, idList = null) {
+		// 	this.products = []
+		// 	let errors, data
+		// 	console.log(type)
+		// 	await fetch("http://192.168.0.115:6969/products/search", {
+		// 		method: "POST",
+		// 		headers: {
+		// 			"Content-Type": "application/json"
+		// 		},
+		// 		body: JSON.stringify({
+		// 			type: type,
+		// 			idList: idList,
+		// 		}),
+		// 	}).then((res) => res.json()).then((jsonRes) => data = jsonRes).catch((err) => errors = err)
+		// 	if (!data.success) return "Eroare interna."
+		// 	if (!data.body.length) return 'Eroare interna'
+		// 	if (idList && idList.length === 1) this.selectedProduct = data.body[0]
+		// 	else this.products = data.body
+		// 	this.availableSizes = data.available_sizes
+		// 	console.log('Found products', this.products)
+		// 	console.log('Found sizes', this.availableSizes)
+		// 	if (errors) return errors
+		// }
+
+		async create(product) {
+			let createError
 		}
 	}
 })
